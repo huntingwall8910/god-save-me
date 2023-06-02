8533333c62deca9cf5c65d14b021d47f8d19d8f4
huntingwall8910 <130617226+huntingwall8910@users.noreply.github.com>
2023-06-02 09:54:24 -0400

    move scripts

--- name.js
+++ name.js
@@ -1,1 +1,8 @@
 document.getElementById("sitename").innerHTML = window.location.hostname;
+function gen() {
+    document.getElementById("rand").innerHTML = Math.floor(Math.random() * 10000000)
+  }
+function peniscumballsacks() {
+    name = document.getElementById("name").value
+    alert("Your name is " + name)
+  }
