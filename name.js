8284734fdcd47fcb43bf8531b13cc8d847579ba4
Stephen Hagenmeyer <130617226+huntingwall8910@users.noreply.github.com>
2023-06-06 16:42:11 -0400

    myspacebardontwork

--- name.js
+++ name.js
@@ -6,20 +6,7 @@ function peniscumballsacks() {
     name = document.getElementById("name").value
     alert("Your name is " + name)
   }
-var coll = document.getElementsByClassName("collapsible");
-var i;
-  
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("activec");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    } 
-  });
-}
+
 const slider = document.getElementById("myRange");
 const wrapper = document.querySelector(".wrapper");
 
