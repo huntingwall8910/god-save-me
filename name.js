b23af9665e613dae9fdb0b31bba59c1fe75fc9d4
Stephen Hagenmeyer <wallhunter9012@gmail.com>
2023-06-16 11:44:02 -0400

    snippet loader

--- name.js
+++ name.js
@@ -1,17 +1,28 @@
-
+//index your number will be
 function gen() {
     document.getElementById("rand").innerHTML = Math.floor(Math.random() * 10000000)
   }
+// name generator
 function peniscumballsacks() {
     name = document.getElementById("name").value
     alert("Your name is " + name)
   }
-
+//colors project
 function change() {
   document.body.style.backgroundColor = document.getElementById("color").value;
   document.getElementById("changebtn").style.color = document.getElementById("color").value;
 }
+//wait until page loads
 document.addEventListener("DOMContentLoaded", function() {
+  //snippet loader
+  fetch('nav.html')
+  .then(response => response.text())
+  .then(data => {
+    // Insert the fetched HTML snippet into the snippet-container
+    document.getElementById('snippet-container').innerHTML = data;
+  })
+  .catch(error => console.log('Error:', error));
+  //draggable divs
   var draggableDivs = document.getElementsByClassName("draggable");
   var isDragging = false;
   var currentDraggableDiv = null;
@@ -39,12 +50,14 @@ document.addEventListener("DOMContentLoaded", function() {
     currentDraggableDiv = null;
   });
 });
+//time
 function updateTime() {
   var currentTime = new Date();
   var options = { hour12: true, hour: 'numeric', minute: 'numeric', second: 'numeric' };
   var time = currentTime.toLocaleTimeString(undefined, options);
   document.getElementById('time').innerHTML = time;
 }
+//kick user if on mobile
 function isMobileDevice() {
   return /Mobi|Android/i.test(navigator.userAgent);
 }
