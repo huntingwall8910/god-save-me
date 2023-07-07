f29fe6145388129da6322786e473072ceb591355
Stephen Hagenmeyer <wallhunter9012@gmail.com>
2023-07-07 18:35:36 -0400

    send help

--- main.js
+++ main.js
@@ -14,7 +14,177 @@ function change() {
 }
 //wait until page loads
 document.addEventListener("DOMContentLoaded", function() {
-  function _0x5dbe(){const _0x6691c2=['16px','position','sleeping','random','div','backgroundImage','1256001nLLaiJ','scratch','1802066MJpDxC','onmousemove','32px','length','createElement','idle','alert','body','81600ojAJia','1404117gWFWXW','1871898rLzGKF','5WHHZfu','left','imageRendering','height','appendChild','style','floor','3116210lnRLnB','sqrt','tired','url(\x27./oneko.gif\x27)','2171776bKgJtU','4upXzIP','px\x20','17IEOash','clientX','pixelated'];_0x5dbe=function(){return _0x6691c2;};return _0x5dbe();}function _0x5cd6(_0x480c40,_0x5be72c){const _0x5dbed3=_0x5dbe();return _0x5cd6=function(_0x5cd640,_0x14cbc7){_0x5cd640=_0x5cd640-0x19c;let _0x4be52a=_0x5dbed3[_0x5cd640];return _0x4be52a;},_0x5cd6(_0x480c40,_0x5be72c);}(function(_0x392704,_0x3d32ed){const _0x3bcba2=_0x5cd6,_0x4eadcd=_0x392704();while(!![]){try{const _0x5167de=-parseInt(_0x3bcba2(0x1af))/0x1*(-parseInt(_0x3bcba2(0x19e))/0x2)+parseInt(_0x3bcba2(0x1b8))/0x3*(-parseInt(_0x3bcba2(0x1ad))/0x4)+-parseInt(_0x3bcba2(0x1a1))/0x5*(-parseInt(_0x3bcba2(0x1a0))/0x6)+parseInt(_0x3bcba2(0x1ba))/0x7+-parseInt(_0x3bcba2(0x1ac))/0x8+parseInt(_0x3bcba2(0x19f))/0x9+-parseInt(_0x3bcba2(0x1a8))/0xa;if(_0x5167de===_0x3d32ed)break;else _0x4eadcd['push'](_0x4eadcd['shift']());}catch(_0x1c96b4){_0x4eadcd['push'](_0x4eadcd['shift']());}}}(_0x5dbe,0x65dfa),function oneko(){const _0x5f1c58=_0x5cd6,_0x1579a1=document[_0x5f1c58(0x1be)](_0x5f1c58(0x1b6));let _0x39d9af=0x20,_0x151a6c=0x20,_0xb66e95=0x0,_0x1e42ed=0x0,_0x5d1fc2=0x0,_0x40f901=0x0,_0x21c6c2=null,_0x3cdd40=0x0;const _0xcb3f5d=0xa,_0x10228c={'idle':[[-0x3,-0x3]],'alert':[[-0x7,-0x3]],'scratch':[[-0x5,0x0],[-0x6,0x0],[-0x7,0x0]],'tired':[[-0x3,-0x2]],'sleeping':[[-0x2,0x0],[-0x2,-0x1]],'N':[[-0x1,-0x2],[-0x1,-0x3]],'NE':[[0x0,-0x2],[0x0,-0x3]],'E':[[-0x3,0x0],[-0x3,-0x1]],'SE':[[-0x5,-0x1],[-0x5,-0x2]],'S':[[-0x6,-0x3],[-0x7,-0x2]],'SW':[[-0x5,-0x3],[-0x6,-0x1]],'W':[[-0x4,-0x2],[-0x4,-0x3]],'NW':[[-0x1,0x0],[-0x1,-0x1]]};function _0x5079a3(){const _0x1bcee7=_0x5f1c58;_0x1579a1['id']='oneko',_0x1579a1[_0x1bcee7(0x1a6)]['width']=_0x1bcee7(0x1bc),_0x1579a1[_0x1bcee7(0x1a6)][_0x1bcee7(0x1a4)]=_0x1bcee7(0x1bc),_0x1579a1[_0x1bcee7(0x1a6)][_0x1bcee7(0x1b3)]='fixed',_0x1579a1[_0x1bcee7(0x1a6)][_0x1bcee7(0x1b7)]=_0x1bcee7(0x1ab),_0x1579a1[_0x1bcee7(0x1a6)][_0x1bcee7(0x1a3)]=_0x1bcee7(0x1b1),_0x1579a1[_0x1bcee7(0x1a6)]['left']=_0x1bcee7(0x1b2),_0x1579a1['style']['top']=_0x1bcee7(0x1b2),document[_0x1bcee7(0x19d)][_0x1bcee7(0x1a5)](_0x1579a1),document[_0x1bcee7(0x1bb)]=_0x474bb1=>{const _0x5a84c9=_0x1bcee7;_0xb66e95=_0x474bb1[_0x5a84c9(0x1b0)],_0x1e42ed=_0x474bb1['clientY'];},window['onekoInterval']=setInterval(_0x4ed7bb,0x64);}function _0x468105(_0x4f79fc,_0x111acc){const _0x506430=_0x5f1c58,_0x1a1231=_0x10228c[_0x4f79fc][_0x111acc%_0x10228c[_0x4f79fc][_0x506430(0x1bd)]];_0x1579a1[_0x506430(0x1a6)]['backgroundPosition']=_0x1a1231[0x0]*0x20+_0x506430(0x1ae)+_0x1a1231[0x1]*0x20+'px';}function _0x3a5077(){_0x21c6c2=null,_0x3cdd40=0x0;}function _0x4d0d07(){const _0x1fa904=_0x5f1c58;_0x40f901+=0x1;_0x40f901>0xa&&Math[_0x1fa904(0x1a7)](Math['random']()*0xc8)==0x0&&_0x21c6c2==null&&(_0x21c6c2=[_0x1fa904(0x1b4),_0x1fa904(0x1b9)][Math[_0x1fa904(0x1a7)](Math[_0x1fa904(0x1b5)]()*0x2)]);switch(_0x21c6c2){case _0x1fa904(0x1b4):if(_0x3cdd40<0x8){_0x468105(_0x1fa904(0x1aa),0x0);break;}_0x468105(_0x1fa904(0x1b4),Math['floor'](_0x3cdd40/0x4));_0x3cdd40>0xc0&&_0x3a5077();break;case _0x1fa904(0x1b9):_0x468105(_0x1fa904(0x1b9),_0x3cdd40);_0x3cdd40>0x9&&_0x3a5077();break;default:_0x468105(_0x1fa904(0x1bf),0x0);return;}_0x3cdd40+=0x1;}function _0x4ed7bb(){const _0xfbd168=_0x5f1c58;_0x5d1fc2+=0x1;const _0x488a03=_0x39d9af-_0xb66e95,_0x1b0d39=_0x151a6c-_0x1e42ed,_0x649410=Math[_0xfbd168(0x1a9)](_0x488a03**0x2+_0x1b0d39**0x2);if(_0x649410<_0xcb3f5d||_0x649410<0x30){_0x4d0d07();return;}_0x21c6c2=null,_0x3cdd40=0x0;if(_0x40f901>0x1){_0x468105(_0xfbd168(0x19c),0x0),_0x40f901=Math['min'](_0x40f901,0x7),_0x40f901-=0x1;return;}direction=_0x1b0d39/_0x649410>0.5?'N':'',direction+=_0x1b0d39/_0x649410<-0.5?'S':'',direction+=_0x488a03/_0x649410>0.5?'W':'',direction+=_0x488a03/_0x649410<-0.5?'E':'',_0x468105(direction,_0x5d1fc2),_0x39d9af-=_0x488a03/_0x649410*_0xcb3f5d,_0x151a6c-=_0x1b0d39/_0x649410*_0xcb3f5d,_0x1579a1[_0xfbd168(0x1a6)][_0xfbd168(0x1a2)]=_0x39d9af-0x10+'px',_0x1579a1[_0xfbd168(0x1a6)]['top']=_0x151a6c-0x10+'px';}_0x5079a3();}());
+  (function oneko() {
+    const nekoEl = document.createElement("div");
+    let nekoPosX = 32;
+    let nekoPosY = 32;
+    let mousePosX = 0;
+    let mousePosY = 0;
+    let frameCount = 0;
+    let idleTime = 0;
+    let idleAnimation = null;
+    let idleAnimationFrame = 0;
+    var nekoSpeed = 10;
+    const spriteSets = {
+        idle: [[-3, -3]],
+        alert: [[-7, -3]],
+        scratch: [
+            [-5, 0],
+            [-6, 0],
+            [-7, 0],
+        ],
+        tired: [[-3, -2]],
+        sleeping: [
+            [-2, 0],
+            [-2, -1],
+        ],
+        N: [
+            [-1, -2],
+            [-1, -3],
+        ],
+        NE: [
+            [0, -2],
+            [0, -3],
+        ],
+        E: [
+            [-3, 0],
+            [-3, -1],
+        ],
+        SE: [
+            [-5, -1],
+            [-5, -2],
+        ],
+        S: [
+            [-6, -3],
+            [-7, -2],
+        ],
+        SW: [
+            [-5, -3],
+            [-6, -1],
+        ],
+        W: [
+            [-4, -2],
+            [-4, -3],
+        ],
+        NW: [
+            [-1, 0],
+            [-1, -1],
+        ],
+    };
+    function zoomies() {
+      var nekoSpeed = 100;
+      setTimeout(zoomies() => {
+        var nekoSpeed = 10
+      }, 10000);
+    }
+    function create() {
+        nekoEl.id = "oneko";
+        nekoEl.style.width = "32px";
+        nekoEl.style.height = "32px";
+        nekoEl.style.position = "fixed";
+        nekoEl.style.backgroundImage = "url('./oneko.gif')";
+        nekoEl.style.imageRendering = "pixelated";
+        nekoEl.style.left = "16px";
+        nekoEl.style.top = "16px";
+
+        document.body.appendChild(nekoEl);
+
+        document.onmousemove = (event) => {
+            mousePosX = event.clientX;
+            mousePosY = event.clientY;
+        };
+
+        window.onekoInterval = setInterval(frame, 100);
+    }
+
+    function setSprite(name, frame) {
+        const sprite = spriteSets[name][frame % spriteSets[name].length];
+        nekoEl.style.backgroundPosition = `${sprite[0] * 32}px ${
+            sprite[1] * 32
+        }px`;
+    }
+
+    function resetIdleAnimation() {
+        idleAnimation = null;
+        idleAnimationFrame = 0;
+    }
+
+    function idle() {
+        idleTime += 1;
+
+        // every ~ 20 seconds
+        if (
+            idleTime > 10 &&
+            Math.floor(Math.random() * 200) == 0 &&
+            idleAnimation == null
+        ) {
+            idleAnimation = ["sleeping", "scratch"][
+                Math.floor(Math.random() * 2)
+            ];
+        }
+
+        switch (idleAnimation) {
+            case "sleeping":
+                if (idleAnimationFrame < 8) {
+                    setSprite("tired", 0);
+                    break;
+                }
+                setSprite("sleeping", Math.floor(idleAnimationFrame / 4));
+                if (idleAnimationFrame > 192) {
+                    resetIdleAnimation();
+                }
+                break;
+            case "scratch":
+                setSprite("scratch", idleAnimationFrame);
+                if (idleAnimationFrame > 9) {
+                    resetIdleAnimation();
+                }
+                break;
+            default:
+                setSprite("idle", 0);
+                return;
+        }
+        idleAnimationFrame += 1;
+    }
+
+    function frame() {
+        frameCount += 1;
+        const diffX = nekoPosX - mousePosX;
+        const diffY = nekoPosY - mousePosY;
+        const distance = Math.sqrt(diffX ** 2 + diffY ** 2);
+
+        if (distance < nekoSpeed || distance < 48) {
+            idle();
+            return;
+        }
+
+        idleAnimation = null;
+        idleAnimationFrame = 0;
+
+        if (idleTime > 1) {
+            setSprite("alert", 0);
+            // count down after being alerted before moving
+            idleTime = Math.min(idleTime, 7);
+            idleTime -= 1;
+            return;
+        }
+
+        direction = diffY / distance > 0.5 ? "N" : "";
+        direction += diffY / distance < -0.5 ? "S" : "";
+        direction += diffX / distance > 0.5 ? "W" : "";
+        direction += diffX / distance < -0.5 ? "E" : "";
+        setSprite(direction, frameCount);
+
+        nekoPosX -= (diffX / distance) * nekoSpeed;
+        nekoPosY -= (diffY / distance) * nekoSpeed;
+
+        nekoEl.style.left = `${nekoPosX - 16}px`;
+        nekoEl.style.top = `${nekoPosY - 16}px`;
+    }
+
+    create();
+  })();
+
   var modal = document.getElementById("Modal");
   var btn = document.getElementById("btn");
   var span = document.getElementsByClassName("close")[0]; 
